3f7fddec93c8f2f8bb93d8248108d34b9c578316
xiaoxin <aishangxing888@gmail.com>
2024-05-30 22:56:31 +0800

    更新

--- app/video/[id]/page.tsx
+++ app/video/[id]/page.tsx
@@ -14,8 +14,8 @@ type Props = {
 };
 // 生成防盗链URL的函数
 function generateAntiTheftUrl(url: string, tokenKey: string) {
-  const nowstamp = Math.floor(Date.now() / 1000);
-  const dutestamp = nowstamp + 10; // 10秒后过期
+  const nowstamp = Date.now(); // 获取当前时间的毫秒数
+  const dutestamp = nowstamp + 30 * 1000; // 60秒后过期
   const playCount = 3; // 允许播放3次
   const tokenUrl = `${url}&counts=${playCount}&timestamp=${dutestamp}${tokenKey}`;
   
